39e7e66b4634e24bd35f70b89e7df67531f37569
alimahad <amahad@macalester.edu>
2017-03-22 14:47:39 -0600

    Checking sun.cpp and .h

--- src/Sun.cpp
+++ src/Sun.cpp
@@ -12,22 +12,19 @@ namespace render {
     
     void init1() {
         if(model1 == NULL){
-        shared_ptr<basicgraphics::Texture> tex = basicgraphics::Texture::create2DTextureFromFile("/Users/Mahad/Downloads/Earthquake/data/earthquakeColors.jpg");
-    tex->setTexParameteri(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    tex->setTexParameteri(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    tex->setTexParameteri(GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-    tex->setTexParameteri(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    tex->setTexParameteri(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        //shared_ptr<basicgraphics::Texture> tex = basicgraphics::Texture::create2DTextureFromFile("/Users/Mahad/Downloads/Earthquake/data/sun.jpg");
         std::vector<std::shared_ptr<basicgraphics::Texture>> textures;
-        vector<shared_ptr<basicgraphics::Texture>> texture;
-            textures.push_back(tex);
+//        vector<shared_ptr<basicgraphics::Texture>> texture;
+//            textures.push_back(tex);
 
-    model1.reset(basicgraphics::Sphere::generate(30,60,textures,0,0.0f,0.5f));
+    model1.reset(basicgraphics::Sphere::generate(15,30,textures,1,0.0f,0.0f));
         
         }
     }
     void draw(basicgraphics::GLSLProgram &shader, mat4 model) {
         vec4 pos(3, -8, 9,1);
+
+        //vec4 pos(0, 1, 0,1);
         pos = glm::rotate(mat4(1), 1.0f, vec3(0, 1, 0))
         * glm::rotate(mat4(1), 1.0f, vec3(0, 0, 1))
         * pos;
@@ -35,21 +32,11 @@ namespace render {
         model = glm::scale(model, vec3(1.2f));
         shader.setUniform("model_mat", model);
         
-        
-        shared_ptr<basicgraphics::Texture> tex = basicgraphics::Texture::create2DTextureFromFile("/Users/Mahad/Downloads/Earthquake/data/earthquakeColors.jpg");
-        tex->setTexParameteri(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        tex->setTexParameteri(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        tex->setTexParameteri(GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-        tex->setTexParameteri(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        tex->setTexParameteri(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        shared_ptr<basicgraphics::Texture> tex = basicgraphics::Texture::create2DTextureFromFile("/Users/Mahad/Downloads/Earthquake/data/earth.jpg");
         vector<shared_ptr<basicgraphics::Texture>> textures;
-        //vector<shared_ptr<basicgraphics::Texture>> texture;
-        float magColor = 1;
-        
-       // _mesh4.reset(basicgraphics::Sphere::generate(30,60,textures,0,magColor,0.5f));
         textures.push_back(tex);
         
-        model1.reset(basicgraphics::Sphere::generate(30,60,textures,0,magColor,0.5f));
+        model1.reset(basicgraphics::Sphere::generate(15,30,textures,1,0.0f,0.0f));
         model1->draw(shader);
     }
     
